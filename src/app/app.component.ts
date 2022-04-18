3ccdb0be49658c7991ca22ddd1308b3bfc5ed05c
KrishnaShinde11 <krishnashinde8000@gmail.com>
2022-04-18 18:29:03 +0530

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding';
+
+  course='javascript'
+  image="https://i.pinimg.com/564x/cb/16/bb/cb16bb284a2a80c75041c80ba63e62d3.jpg"
+
+  goBack(): void {}
+
+  msg:any="my message"
 }
